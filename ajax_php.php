0815dcfb2c6a02b67b1690a4ab4948a88501e2a3
Atuxe Young <hdb@boyuntong.com>
2014-09-02 17:43:02 +0800

    v1

--- ajax_php.php
+++ ajax_php.php
@@ -1,6 +1,10 @@
 <?php
-     header('Content-Type:text/html; charset=gb2312');//使用gb2312编码，使中文不会变成乱码
-     $backValue=$_POST['trans_data'];
+    // header('Content-Type:text/html; charset=gb2312');//使用gb2312编码，使中文不会变成乱码
+     //$backValue=$_POST['trans_data'];
      //echo $backValue."+后台返回";
-     echo (rand(0,10));
+
+     $r = new COM('KLCOM_N1000.DataTrans');
+     $data = $r->GetData('zd');
+     echo $data;
+     //echo (rand(0,10));
  ?>
